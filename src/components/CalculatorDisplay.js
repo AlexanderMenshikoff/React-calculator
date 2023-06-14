33b5469aa0fcba4fffd369c821d428728e86575e
Alex_Menshikoff <menshikov.sasha2015@yandex.ru>
2023-06-15 01:31:00 +0500

    changed styles for chakra Box component, added SidePanel component and its props

--- src/components/CalculatorDisplay.js
+++ src/components/CalculatorDisplay.js
@@ -1,9 +1,17 @@
 import { Box } from "@chakra-ui/react";
 import Numbers from "./Numbers";
 import InputWindow from "./InputWindow";
+import SidePanel from "./SidePanel";
+
 const CalculatorDisplay = (props) => {
   return (
-    <Box display="flex" justifyContent="center" alignItems="center" h="100vh">
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      h="100vh"
+      flexDirection="column"
+    >
       <Box
         display="flex"
         gap="5px"
@@ -14,9 +22,16 @@ const CalculatorDisplay = (props) => {
         background="gray.400"
         h="300px"
         borderRadius="15px"
+        flexWrap="wrap"
       >
         <InputWindow counts={props.counts} result={props.result} />
         <Numbers onClick={props.setCounts} counts={props.counts} />
+        <SidePanel
+          counts={props.counts}
+          result={props.result}
+          setResult={props.setResult}
+          setCounts={props.setCounts}
+        />
       </Box>
     </Box>
   );
