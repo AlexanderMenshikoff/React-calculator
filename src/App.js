36d31ffd4d6aabf90a474a33610f90e50e236469
Alex_Menshikoff <menshikov.sasha2015@yandex.ru>
2023-06-14 20:09:34 +0500

    added CalculatorDisplay component instead of Numbers component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { Box, Text, Button } from "@chakra-ui/react";
 import Numbers from "./components/Numbers";
 import { useState } from "react";
+import CalculatorDisplay from "./components/CalculatorDisplay";
 
 const App = () => {
   const [counts, setCounts] = useState(0);
   const [result, setResult] = useState("");
   return (
     <div className="App">
-      <Numbers counts={counts} result={result} />
+      <CalculatorDisplay
+        counts={counts}
+        result={result}
+        setCounts={setCounts}
+      />
     </div>
   );
 };
