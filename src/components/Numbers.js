7c4b9c8e1e72a631dedf3f86ba51351be29a5f23
Alex_Menshikoff <menshikov.sasha2015@yandex.ru>
2023-06-15 01:27:14 +0500

    deleted Text component from chakra

--- src/components/Numbers.js
+++ src/components/Numbers.js
@@ -1,4 +1,4 @@
-import { Box, Button, Text } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 
 const Numbers = (props) => {
   const nums = Array.from(Array(10).keys()).map((number) => {
