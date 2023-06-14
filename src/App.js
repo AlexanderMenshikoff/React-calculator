d0934fa47135149515a5476c4655bf315cf799aa
Alex_Menshikoff <menshikov.sasha2015@yandex.ru>
2023-06-14 17:42:48 +0500

    added new component + styles from chakra

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Box, Text, Button } from "@chakra-ui/react";
+import Numbers from "./components/Numbers";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="App">
+      <Box display="flex" justifyContent="center" alignItems="center" h="100vh">
+        <Numbers />
+      </Box>
+    </div>
+  );
 };
 
 export default App;
