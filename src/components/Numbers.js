1c1e18e76660ba647e44619470d0b33cf8a7dda9
Alex_Menshikoff <menshikov.sasha2015@yandex.ru>
2023-06-14 18:28:48 +0500

    added input style, accepted props

--- src/components/Numbers.js
+++ src/components/Numbers.js
@@ -1,16 +1,54 @@
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Button, Text } from "@chakra-ui/react";
 
 const Numbers = (props) => {
   const nums = Array.from(Array(10).keys()).map((number) => {
     return (
-      <Button key={number} w="40px" h="40px" margin="4px">
+      <Button key={number} w="45px" h="45px" margin="4px">
         {number}
       </Button>
     );
   });
   return (
-    <Box display="flex" flexWrap="wrap" w="9%">
-      {nums}
+    <Box display="flex" justifyContent="center" alignItems="center" h="100vh">
+      <Box
+        display="flex"
+        gap="5px"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        w="200px"
+        background="gray.400"
+        h="300px"
+        borderRadius="15px"
+      >
+        <Box display="flex" w="100%" justifyContent="between">
+          <Text
+            display="flex"
+            justifyContent="start"
+            alignItems="center"
+            bg="gray.50"
+            w="100%"
+            h="38px"
+            px="4px"
+            borderRadius="8px"
+            margin="5px"
+          >
+            {props.counts}
+          </Text>
+          <Text w="fit-content" h="38px" textColor="tomato">
+            {props.result}
+          </Text>
+        </Box>
+        <Box
+          display="flex"
+          flexWrap="wrap"
+          w="80%"
+          margin="10px"
+          borderRadius="10px"
+        >
+          {nums}
+        </Box>
+      </Box>
     </Box>
   );
 };
